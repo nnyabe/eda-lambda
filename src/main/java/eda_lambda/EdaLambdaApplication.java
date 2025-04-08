319d5bf9eefab5f021e00f6d9bf47134969eefed
nnyabe.chamamme <nnyabe.chamamme@amalitech.com>
2025-04-08 13:47:40 +0000

    Created: Project and all files in it

---
+++ src/main/java/eda_lambda/EdaLambdaApplication.java
@@ -0,0 +1,13 @@
+package eda_lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdaLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eda_lambda/EdaLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package eda_lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
